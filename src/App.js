70a074b49b8b1ce71c550be7123f7028a37217ae
Mikhaylov-Andrey-derek23 <derek23@yandex.ru>
2021-07-29 16:38:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import './style/style.scss';
+import Congratulations from './components/Congratulations'
+import Card from './components/Card';
+
+
+function App() {
+  const [data, changeData] = useState([]);
+
+  useEffect(()=>{
+    fetch('app/data.json').then(result => {
+      return result.json()
+    }).then(data => {
+      changeData(data)
+    }) 
+  }, [])
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-center my-3">Ольга,<br/> С Днем Рождения!</h1>
+      </header>
+      <Congratulations/>
+      <h2 className="text-center py-3">
+        Подарок в День рождения твоя детская песня!
+      </h2>
+      {
+        data.length > 0 ?data.map( (e, index)=> <Card data={e} key={index}/>) : ''
+      }
+    </div>
+  );
+}
+
+export default App;
